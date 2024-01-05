6ea91481a829182faace25b7c1ac3f3155bbeab5
julialozmin <julialozmin@gmail.com>
2024-01-05 12:05:09 +0100

    Created the basic React app structure and installed Axios and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Card defaultCity="Chieti" />
+
+        <small>
+          Coded by{" "}
+          <a
+            href="http://www.linkedin.com/in/julia-lozoya-25a400146"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Julia Lozoya
+          </a>
+          , available open source on{" "}
+          <a
+            href="https://github.com/julialozmin/ReactWeatherApp.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">
+            Netlify
+          </a>
+        </small>
+      </div>
     </div>
   );
 }
