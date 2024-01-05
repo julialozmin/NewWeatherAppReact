feb025e742f00bcef56bb96cdd08a1fd0fa3302e
julialozmin <julialozmin@gmail.com>
2024-01-05 12:38:40 +0100

    Updated the temperature units and value

--- src/CityDetails.js
+++ src/CityDetails.js
@@ -1,13 +1,16 @@
 import React from "react";
-import TemperatureUnits from "./TemperatureUnits";
 import "./CityDetails.css";
 
 export default function CityDetails(props) {
+  let roundedCelsius = Math.round(props.data.temperature);
   return (
     <div className="CityDetails">
       <h2>{props.data.city}</h2>
       <h6 className="text-capitalize">{props.data.weatherDescription}</h6>
-      <TemperatureUnits celsius={props.data.temperature} />
+      <h1>
+        {roundedCelsius}
+        <span className="opacity-75">º C</span>
+      </h1>
     </div>
   );
 }
